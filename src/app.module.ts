d0d29ab68444468e4d9a4f308a6541a7e45fce00
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-08-24 12:30:40 +0200

    👨 generate user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './routes/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './routes/user/user.module';
 import auth from './config/auth.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [auth] }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [auth] }), AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
