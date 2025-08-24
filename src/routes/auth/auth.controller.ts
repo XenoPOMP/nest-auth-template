4388aee0b89151f7ada7161175ac3eb7038640b9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-08-24 14:23:45 +0200

    📝 change status code

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { Endpoint } from '../../decorators/endpoint.decorator';
@@ -9,6 +9,7 @@ export class AuthController {
 
   @Endpoint('POST', 'register', {
     authRequired: false,
+    httpCode: HttpStatus.CREATED,
   })
   async register(@Body() dto: AuthDto) {
     return this.authService.register(dto);
